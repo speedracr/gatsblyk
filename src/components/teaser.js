c9c9dd680f5c99dc42e78d55e44aac2ad88db668
Jonas Knipper <jonas@robocomobo.com>
2021-11-11 21:49:19 +0100

    Add dynamicComponent

---
+++ src/components/teaser.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Teaser = ({ blok }) => (
+  <div>
+    <h2>
+      { blok.headline }
+    </h2>
+  </div>
+)
+
+export default Teaser
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,25 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import { graphql } from "gatsby"
+import SbEditable from 'storyblok-react'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import DynamicComponent from "../components/dynamicComponent"
 
-const IndexPage = ({ data }) =>
-  {
-    const story = data.storyblokEntry
-    const content = JSON.parse(story.content)
-    console.log(content)
+const IndexPage = ({ data, location }) => {
+  let story = data.storyblokEntry
+  const content = JSON.parse(story.content)
+
+  const components = content.body.map(blok => {
+    return (<DynamicComponent blok={blok} key={blok._uid} />)
+  })
 
     return (
   <Layout>
     <Seo title="Home" />
     <h1>{ content.title }</h1>
+    { components }
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
